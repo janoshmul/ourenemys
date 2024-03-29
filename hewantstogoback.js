ae009407aad8c62c96dae117e6ed4dc17394392e
janoshmul <76818533+janoshmul@users.noreply.github.com>
2024-03-29 21:28:06 +0100

    Add files via upload

---
+++ hewantstogoback.js
@@ -0,0 +1,18 @@
+function HE_WANTS_TO_GO_BACK(){
+    alert("are u sure u want to go back ??");
+    alert("click the btn again to confirm");
+}
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
